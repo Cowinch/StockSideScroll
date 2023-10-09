22a33bbe34c1ae627dad4d95c6657e1deea1fe4c
Cole Winchester <cowinch@gmail.com>
2023-10-08 20:07:27 -0500

    infinite side scroller complete

--- script.js
+++ script.js
@@ -2,11 +2,22 @@ const scrollers = document.querySelectorAll('.scroller')
 
 //ctrl+shift+P and then run 'emulate CSS prefers-reduced-motion: reduce' to test
 if(!window.matchMedia("(prefers-reduced-motion: reduce)").matches){
-    addAnimation();
+    addAnimation()
 }
 
 function addAnimation(){
     scrollers.forEach(scroller => {
-        scrollers.setAttribute('data-animated', true)
+        scroller.setAttribute('data-animated', true)
+        console.log(scroller)
+
+        const scrollerInner = scroller.querySelector('.scroller-inner')
+        const scrollerContent = Array.from(scrollerInner.children);
+
+        scrollerContent.forEach(item => {
+            const duplicatedItem = item.cloneNode(true)
+            //aria-hidden prevents screen readers from reading the scroller twice
+            duplicatedItem.setAttribute('aria-hidden', true)
+            scrollerInner.appendChild(duplicatedItem)
+        })
     })
 }
